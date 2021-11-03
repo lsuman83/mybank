f7c2137758181b2fe20e8e0055b36d00a202ab03
lsuman83 <sean.j.bordelon@gmail.com>
2021-11-03 11:27:25 -0400

    Combining the forms and display components and adding styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import Display from './Display';
+import { useState } from 'react'
+import Form from './Form';
 
 function App() {
+  const [date, setDate] = useState('')
+  const [desc, setDesc] = useState('')
+  const [amount, setAmount] = useState('')
+  const [options, setOption] = useState(["debit", "credit"])
+  const [choice, setChoice] =  useState('credit')
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setAmount(Number(e.target.elements.amount.value))  
+    setDate(e.target.elements.date.value)
+    setDesc(e.target.elements.description.value)
+  }
+
+  const selected = (e) => {
+    e.preventDefault()
+    setChoice(e.target.elements.options.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={selected}>
+        <select id="options" name="options">
+          <option value="credit">Credit</option>
+          <option value="debit">Debit</option>
+        </select>
+        <input type="submit"/>
+      </form>
+      {choice === 'debit' ?
+            (<h1>Payment</h1>) : (<h1>Deposit</h1>)}
+      <Form onSubmit={onSubmit}/>
+      <hr></hr>
+      <Display choice={choice} date={date} description={desc} amount={amount} /> 
     </div>
   );
 }
